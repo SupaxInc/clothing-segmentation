Bug fix: scaling images went over 0-255 grayscale value

data/dataset.py
@@ -25,7 +25,7 @@ class ClothingCoParsingDataset(Dataset):
         image = np.array(Image.open(self.image_paths[idx]).convert('RGB')) # Converts PIL image to numpy array, each pixel will be 0 to 255
         mask = np.array(Image.open(self.mask_paths[idx]).convert('L'), dtype=np.uint8)  # Convert to grayscale, pixels 0 to 255 (whiteness)
         # Convert scaled mask back to original class labels
-        mask = np.round(mask / 43).astype(np.uint8)
+        mask = np.round(mask / 36).astype(np.uint8)
 
         if self.transform is not None:
             augmentations = self.transform(image=image, mask=mask)

scripts/pre_processing.py
@@ -16,6 +16,9 @@ def remap_annotation_classes(annotation):
         for original in originals:
             remapped_annotation[annotation == original] = new
     
+    unique, counts = np.unique(remapped_annotation, return_counts=True)
+    print(dict(zip(unique, counts)))
+
     return remapped_annotation
 
 def convert_mat_to_png(mat_directory, output_directory):
@@ -37,10 +40,9 @@ def convert_mat_to_png(mat_directory, output_directory):
             
             remapped_annotation = remap_annotation_classes(annotation)
             
-            # TODO: Change this so our mask is RGB instead of grayscale
             # Convert annotation matrix to an Image
-                # Scale remapped values from 0-5 by 36 to match grayscale value range 0-255 (making it brighter)
-            img = Image.fromarray((remapped_annotation * 43).astype(np.uint8))
+                # Scale remapped values from 0-6 by 36 to match grayscale value range 0-255 (making it brighter)
+            img = Image.fromarray((remapped_annotation * 36).astype(np.uint8))
             img.save(os.path.join(output_directory, filename.replace('.mat', '.png')))
     
 def split_data(base_image_path, base_mask_path, train_size=0.8):